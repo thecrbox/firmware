aedc642fa14e82af3a9f2f376dfc648af842c345
Michal Stawinski <michal.stawinski@ext.panasonicautomotive.com>
2026-04-04 03:42:17 +0200

    Hide advanced stuff by default

---
+++ src/ui_helpers.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "esphome/components/time/real_time_clock.h"
+
+namespace helpers {
+
+inline bool is_day_mode(esphome::ESPTime time, esphome::ESPTime day_start, esphome::ESPTime night_start) {
+    if (!time.is_valid()) {
+        return true; // Assume day mode if time is not valid
+    }
+    if (day_start.hour < night_start.hour || (day_start.hour == night_start.hour && day_start.minute < night_start.minute)) {
+        // Normal case: day starts before night (e.g., 07:00-22:00)
+        return (time.hour > day_start.hour || (time.hour == day_start.hour && time.minute >= day_start.minute)) &&
+               (time.hour < night_start.hour || (time.hour == night_start.hour && time.minute < night_start.minute));
+    } else {
+        // Special case: day starts after night (overnight, e.g., 22:00-07:00 is night)
+        return (time.hour > day_start.hour || (time.hour == day_start.hour && time.minute >= day_start.minute)) ||
+               (time.hour < night_start.hour || (time.hour == night_start.hour && time.minute < night_start.minute));
+    }
+}
+
+} // namespace helpers
